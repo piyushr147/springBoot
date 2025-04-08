fdbc2e00342fa916688d762b5f5bad283ed27915
piyushr147 <piyush.rawat@shl.com>
2025-04-08 16:18:42 +0530

    updated identity entity class .

--- springJPA/src/main/java/com/spring/springJPA/entity/Identity.java
+++ springJPA/src/main/java/com/spring/springJPA/entity/Identity.java
@@ -1,10 +1,9 @@
 package com.spring.springJPA.entity;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.spring.springJPA.enums.IdentityType;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -20,15 +19,21 @@ public class Identity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     @Column(nullable = false)
+    @Size(message = "Identification number cannot be less than 8 and greater than 16", min = 8,max = 16)
     private String number;
+
     @Column(nullable = false)
     private IdentityType identityType;
-    @OneToOne(fetch = FetchType.EAGER,mappedBy = "identity")
+
+    @OneToOne(mappedBy = "identity")
     @JsonIgnoreProperties("identity")
     private Student student;
+
     @CreationTimestamp
     private LocalDateTime createDateTime;
+
     @UpdateTimestamp
     private LocalDateTime updateDateTime;
 
