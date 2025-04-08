ab40a1d70d51efdda4f23a9c4b4793ceb73af481
piyushr147 <piyush.rawat@shl.com>
2025-04-08 16:09:52 +0530

    add and update api in review service and controller.

--- springJPA/src/main/java/com/spring/springJPA/entity/Review.java
+++ springJPA/src/main/java/com/spring/springJPA/entity/Review.java
@@ -1,11 +1,13 @@
 package com.spring.springJPA.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -17,36 +19,35 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
+@JsonIgnoreProperties(value = {"course"})
 public class Review implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     @Column(nullable = false)
+    @Min(value = 1,message = "Rating cannot be less than 1.")
+    @Max(value = 5, message = "Rating cannot be more than 5.")
     private int rating;
-    @ManyToOne(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(nullable = false)
     @JsonBackReference
     private Course course;
+
+    @NotBlank(message = "Review description cannot be empty")
+    @Size(message = "Review description cannot be less than 4 and more than 30 characters", min = 4,max = 30)
     private String description;
+
     @CreationTimestamp
     private LocalDateTime createDateTime;
+
     @UpdateTimestamp
     private LocalDateTime updateDateTime;
 
     public Review(int rating,String description){
         this.rating = rating;
         this.description = description;
-    }
-
-    @Override
-    public String toString() {
-        return "Review{" +
-                "id=" + id +
-                ", rating=" + rating +
-                ", course=" + course +
-                ", description='" + description + '\'' +
-                ", createDateTime=" + createDateTime +
-                ", updateDateTime=" + updateDateTime +
-                '}';
     }
 }
